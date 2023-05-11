f1f15993d6d49b5427c868b3d18aa90d66524911
mercylin muthoni <linmercymuthoni@gmail.com>
2023-05-11 22:15:08 +0300

    up and running

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center 
+      items-center flex-col'>
+      <nav className="flex justify-between 
+        items-center w-full mb-10 pt-3 ">
+        <img src={logo} alt="sumz_logo" 
+        className="w-28 object-contain"/>
+
+        <button type='button' onClick={() => window.open('https://github.com/linmercy/AI-summerizer.git')} 
+        className='black_btn'>
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summerize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>
+          OpenAI GPT-4
+        </span>
+      </h1>
+
+      <h2 className='desc'>
+        Simplify your reading with Sumz, an open source article summerizer that
+        transforms lengthy articles into clear and concise summaries. 
+      </h2>
+    </header>
   )
 }
 
